fdc8e3bb571e9c3da61d5be0af6576b36bd9270c
Kholdan <darrell@spinler.net>
2018-07-15 16:28:33 +0000

    Updated to python 3
    .

--- learninglogic.py
+++ learninglogic.py
@@ -9,9 +9,9 @@ triggers2 = "office";
 while utterance != "exit":
     if i is 1:
         if (any(trigger in utterance for trigger in triggers1)) and (triggers2 in utterance):
-            print "Now starting up the office\n";
+            print ("Now starting up the office\n");
         else:
-            print "Command not recognised\n";
+            print ("Command not recognised\n");
     else:
         i = i + 1;
-    utterance = raw_input("K2S0: ");
+    utterance = input("K2S0: ");
