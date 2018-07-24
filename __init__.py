48ff98fed1353126e22518fec17b67f52dacd8bd
Kholdan <darrell@spinler.net>
2018-07-24 21:31:50 +0000

    Attempt 2

--- __init__.py
+++ __init__.py
@@ -16,37 +16,58 @@
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
 
 from adapt.intent import IntentBuilder
-from mycroft import intent_handler
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
 
-from mycroft.skills.core import MycroftSkill
-from mycroft.util.log import getLogger
+# Each skill is contained within its own class, which inherits base methods
+# from the MycroftSkill class.  You extend this class as shown below.
 
-__author__ = 'Kholdan'
-
-LOGGER = getLogger(__name__)
-
-
+# TODO: Change "Template" to a unique name for your skill
 class WOLSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(WOLSkill, self).__init__(name="WOLSkill")
         
-    @intent_handler(IntentBuilder("WOL_Intent").require("WOLKeyword").require("TargetKeyword"))
+        # Initialize working variables used within the skill.
+        self.count = 0
+
+    # The "handle_xxxx_intent" function is triggered by Mycroft when the
+    # skill's intent is matched.  The intent is defined by the IntentBuilder()
+    # pieces, and is triggered when the user's utterance matches the pattern
+    # defined by the keywords.  In this case, the match occurs when one word
+    # is found from each of the files:
+    #    vocab/en-us/Hello.voc
+    #    vocab/en-us/World.voc
+    # In this example that means it would match on utterances like:
+    #   'Hello world'
+    #   'Howdy you great big world'
+    #   'Greetings planet earth'
+    @intent_handler(IntentBuilder("").require("WOL"))
     def handle_WOL_intent(self, message):
-        LOGGER.debug('WOL intent loaded')
-        #TODO Create config file and initalize list of MAC addresses from file
-        
-        target = message.data.get("TargetKeyword")
-        
+        # In this case, respond by simply speaking a canned response.
+        # Mycroft will randomly speak one of the lines from the file
+        #    dialogs/en-us/hello.world.dialog
+        self.speak_dialog("starting")
 
-        if target = "office":
-            self.speak_dialog("starting")
-            #TODO Add logic to send WOL packet to Specific MAC address on network
-        else:
-            self.speak_dialog("unable")
+    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
+    def handle_count_intent(self, message):
+        if message.data["Dir"] == "up":
+            self.count += 1
+        else:  # assume "down"
+            self.count -= 1
+        self.speak_dialog("count.is.now", data={"count": self.count})
 
-    def stop(self):
-        pass
+    # The "stop" method defines what Mycroft does when told to stop during
+    # the skill's execution. In this case, since the skill's functionality
+    # is extremely simple, there is no need to override it.  If you DO
+    # need to implement stop, you should return True to indicate you handled
+    # it.
+    #
+    # def stop(self):
+    #    return False
 
-
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
 def create_skill():
     return WOLSkill()
