40f5d436be9e48710913db3a755e5d1876184f87
Kholdan <darrell@spinler.net>
2018-07-25 01:07:24 +0000

    Testing WOL

--- __init__.py
+++ __init__.py
@@ -71,7 +71,7 @@ class WOLSkill(MycroftSkill):
             LOG.debug("Game server code running")
         elif message.data["Target"] == "storage server":
             self.speak_dialog("starting", data={"Target": target})
-            magicpacket = '\xFF\xFF\xFF\xFF\xFF\xFF' + '\x00\x23\x7D\x60\xCD\x24' * 16
+            magicpacket = 'FFFFFFFFFFFF' + '00237D60CD24' * 16
             
             sock = socket(AF_INET, SOCK_DGRAM)
             sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
